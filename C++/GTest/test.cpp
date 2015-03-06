e0abc413f9b88609174a999fdc716ee896dc971d
yuandaxing <mryuan0@gmail.com>
2015-03-06 11:00:13 +0800

    add gtest demo

---
+++ C++/GTest/test.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+
+TEST(blahTest, blah1) {
+    EXPECT_EQ(1, 1);
+}
+
+int main (int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    int returnValue;
+
+    //Do whatever setup here you will need for your tests here
+    //
+    //
+
+    returnValue =  RUN_ALL_TESTS();
+
+    //Do Your teardown here if required
+    //
+    //
+
+    return returnValue;
+}
