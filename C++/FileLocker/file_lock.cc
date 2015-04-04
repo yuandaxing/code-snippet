e3a3b0f7f85ab14a2765422b1c2f9e232c217824
yuandaxing <1179923349@qq.com>
2015-04-04 17:35:33 +0800

    file locker

---
+++ C++/FileLocker/file_lock.cc
@@ -0,0 +1,82 @@
+#include "file_lock.h"
+#include <fcntl.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <iostream>
+#include <unistd.h>
+
+namespace MicroAd
+{
+namespace Utils
+{
+FileLocker::FileLocker(const std::string& filePath): path_(filePath), \
+                                                     fd_(-1){}
+bool FileLocker::Open()
+{
+  //fcntl.h
+  const int lock_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+  fd_ = open(path_.c_str(), O_RDWR | O_CREAT, lock_mode);
+  if (fd_ < 0)
+  {
+    std::cout << "open lock file error";
+  }
+  return fd_ > 0;
+}
+
+int FileLocker::TryLock()
+{
+  if(0 > fd_)
+  {
+    return 2;
+  }
+  struct flock file_lock;
+  file_lock.l_type = F_WRLCK;
+  file_lock.l_start = 0;
+  file_lock.l_whence = SEEK_SET;
+  int ret = fcntl(fd_, F_SETLK, &file_lock);
+  if(ret != 0)
+  {
+    if(errno == EAGAIN || errno == EACCES)
+    {
+      return 1;
+    }else
+    {
+      return 2;
+    }
+  }
+  return 0;
+}
+
+bool FileLocker::UnLock()
+{
+  if(fd_ < 0)
+  {
+    return false;
+  }
+  struct flock file_lock;
+  file_lock.l_type = F_UNLCK;
+  file_lock.l_start = 0;
+  file_lock.l_whence = SEEK_SET;
+  return 0 == fcntl(fd_, F_SETLK, &file_lock);
+}
+
+bool FileLocker::TruncWrite(const std::string& content)
+{
+  if(fd_ < 0)
+  {
+    return false;
+  }
+  if(ftruncate(fd_, 0) != 0)
+  {
+    return -1;
+  }
+  if(write(fd_, content.c_str(), content.size()) != content.size())
+  {
+    return -1;
+  }
+  return 0;
+}
+
+}
+}
