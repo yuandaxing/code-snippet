b6e8c6ed7119c60dcb0e1375c22bd5025392c315
yuandaxing <mryuan0@gmail.com>
2015-06-11 19:51:14 +0800

    backup interactive writing

--- shell/backup_run.sh
+++ shell/backup_run.sh
@@ -44,5 +44,55 @@ change_settings()
     do
         echo -n "Mail Admin When Done"
         read T_INFORM
-        : {}
+        : ${T_INFORM:$_INFROME}
+        case $T_INFORM in
+            yes|Yes) break;;
+            no|No) break;;
+            *) echo "the choices are yes, no";;
+        esac
+    done
+
+    while :
+    do
+        echo -n "Backup Type               [$_TYPE]:"
+        read T_TYPE
+        : ${T_TYPE:=$_TYPE}
+        case $T_TYPE in
+            Full|full) break;;
+            Normal|normal)break;;
+            Sybase|sybase)break;;
+            *)echo "The choices are either ... full, normal, sybase"
+                ;;
+            esac
+    done
+    _DEVICE=$T_DEVICE; _INFROME=$T_INFORM; _TYPE=$T_TYPE;
+}
+
+show_settings()
+{
+    cat <<EOF
+                  Default Settings Are ...
+                  Tape Device To Be Used : $_DEVICE
+                  Mail Admin When Done   : $_INFROME
+                  Type Of Backup         : $_TYPE
+                  Log file of backup     : $_LOGFILE
+EOF
+}
+
+get_code()
+{
+    clear
+    header
+    _COUNTER=0
+    echo " You Must enter the correct code to be able change
+           default settings"
+    while
+    do
+        _COUNTER=`expr $_COUNTER + 1`
+        echo -n "Enter the code to change the settings: "
+        read T_CODE
+        if [ "$T_CODE" = "$_CODE" ] ; then
+            return 0
+        fi
+    done
 }
